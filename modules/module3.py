bf39440f89c7b5cb25bd758a13f5350f683238d8
manavsainani <sainanikrish@gmail.com>
2025-04-25 13:27:31 -0400

    module 3

--- modules/module3.py
+++ modules/module3.py
@@ -1,8 +1,11 @@
+# Module 3: ""
+# Requires the user to hold down the button while completing another part of the bomb
+# letting go of the button too early will remove time from the counter
 from modules.aModule import aModule
 
 
 class module3(aModule):
     def __init__(self):
         super().__init__()
-        self.name = ''
+        self.name = 'Mine'
 
