589c1eaba5020a8545c2a7a560a823b5e0583ab4
Dallas-A26 <dfapperson@gmail.com>
2025-04-28 13:39:33 -0400

    making my module work

---
+++ modules/module4.py
@@ -0,0 +1,27 @@
+"""
+Reverses what the combination is but doesn't show what you put it shows the opposite
+
+"""
+from modules.aModule import aModule
+
+global solveProgress
+
+class Module4(aModule):
+    def __init__(self):
+        super().__init__()
+        self.name = "Shenanigans"
+
+    def solve(self) -> bool:
+        if solveProgress==4:
+            return True
+        else:
+            return False
+    def update(self, switches, button, wires, keypad, timer, screen):
+        if wires == "01101":
+            solveProgress += 1
+        if switches == "1010":
+            solveProgress += 1
+        if button == button._pressed():
+            solveProgress += 1
+        if keypad == "1234":
+            solveProgress += 1
