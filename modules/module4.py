7e3872af8c18aae3352554fc235f5c196555bbd2
Dallas-A26 <dfapperson@gmail.com>
2025-05-03 00:56:18 -0400

    editing the GUI to show the values

--- modules/module4.py
+++ modules/module4.py
@@ -66,6 +66,6 @@ class module4(aModule):
             keypad._defused = True
 
         screen._lwires.config(text=f"Wires:{self.invert(wires._value)}")
-        screen._ltoggles.config(text=f"Toggles:{switches._value}")
+        screen._ltoggles.config(text=f"Toggles:{self.invert(switches._value)}")
         screen._lkeypad.config(text=f"Keypad:{keypad._value}")
         screen._lbutton.config(text=f"Button:{'Pressed' if button._pressed else 'Not pressed'}")
