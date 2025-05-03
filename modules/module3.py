730780451d04f011abb57cc9080d91bd8f2c9449
manavsainani <sainanikrish@gmail.com>
2025-05-03 18:16:27 -0400

    module 3

--- modules/module3.py
+++ modules/module3.py
@@ -92,7 +92,7 @@ class module3(aModule):
             self._defused = True
             screen.hide_all()
             screen.replace_all()
-            self.time_held.grid_forget()
+            self.time_held.destroy()
             self.game_desc.destroy()
             self.description.destroy()
             self.title_Label.grid_forget()
