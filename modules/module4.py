d21ceb13458e601add1fe89c8e131e8a2fb5f2eb
Dallas-A26 <dfapperson@gmail.com>
2025-05-03 16:37:08 -0400

    figuruing out if solve is working or not

--- modules/module4.py
+++ modules/module4.py
@@ -81,7 +81,7 @@ class module4(aModule):
         #shows the inverted value for wires and toggles while displaying all values constantly
         screen._lwires.config(text=f"Wires:{self.invert(wires._value)} (target: {expected_wires})")
         screen._ltoggles.config(text=f"Toggles:{self.invert(switches._value)} (target: {expected_switches})")
-        screen._lkeypad.config(text=f"Keypad:{keypad._value} (target: {keypad._target})")
+        screen._lkeypad.config(text=f"Keypad:{keypad._value} (target: {expected_keypad})")
         screen._lbutton.config(text=f"Button:{'Pressed' if button._pressed else 'Not pressed'}")
         self._solve.config(text=f"Solve Progress: {self.solve_progress}")
 
